abc8ed973360a880e4ea59e38d366ea2c8993607
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2024-02-13 22:02:55 +0100

    Feature : add edit author

--- src/app/components/author-edit-page/author-edit-page.component.ts
+++ src/app/components/author-edit-page/author-edit-page.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Author } from '../../interfaces/author';
+import { AuthorsService } from '../../services/authors.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SimpleAuthor } from '../../interfaces/simple-author';
 
 @Component({
   selector: 'app-author-edit-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './author-edit-page.component.html',
-  styleUrl: './author-edit-page.component.css'
+  styleUrl: './author-edit-page.component.css',
 })
 export class AuthorEditPageComponent {
+  author: SimpleAuthor = {
+    id: 0,
+    nom: '',
+    prenom: '',
+    email: '',
+    telephone: '',
+    grade: '',
+  };
 
+  err: number = 0;
+
+  constructor(
+    private authorsService: AuthorsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.authorsService
+      .getSimple(id)
+      .subscribe((author) => (this.author = author));
+  }
+
+  onSubmit() {
+    const author: SimpleAuthor = {
+      id: this.author.id,
+      nom: this.author.nom,
+      prenom: this.author.prenom,
+      email: this.author.email,
+      telephone: this.author.telephone,
+      grade: this.author.grade,
+    };
+    this.authorsService.put(author.id, author).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.router.navigate([`authors/${author.id}`]);
+      },
+      error: (err: any) => {
+        this.err = 1;
+      },
+    });
+  }
 }
