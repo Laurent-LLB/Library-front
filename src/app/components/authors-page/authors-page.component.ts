f46afdf83f4438689e77cdc23f976893624d07ad
Laurent LB <llb.leborgne@gmail.com>
2024-02-13 14:10:10 +0100

    first css

--- src/app/components/authors-page/authors-page.component.ts
+++ src/app/components/authors-page/authors-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-authors-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './authors-page.component.html',
   styleUrl: './authors-page.component.css'
 })
